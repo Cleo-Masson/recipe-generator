5f76a644ae403d09f209a0dbb4da59ab0472fdff
Cleo-Masson <cleonikita50@gmail.com>
2024-09-19 14:31:29 +0400

    Improved the user experience. Updated the css elements

--- src/index.js
+++ src/index.js
@@ -14,8 +14,12 @@ function generateRecipe(event) {
   let apiKey = "at12a73f4a384odff22ab003eefa9af8";
   let prompt = `User instructions: Generate a quick whole foods recipe ${userInstructionsInput.value} .`;
   let context =
-    "You are an expert in nutrition and whole food recipes AI assistant. You goal is give the user a recipe recommendation of 7 lines that is budget friendly and quick and easy. display the recipe with a title in a strong element, seperate each line with <br>. Make sure that you follow the user's instructions.";
+    "You are an expert in nutrition and whole food recipes AI assistant. You goal is give the user a recipe recommendation of 7 lines that is budget friendly and quick and easy. display the recipe with a title in a strong element, seperate each line with <br>. Make sure that you follow the user's instructions. Seperate the <strong>ingredients</strong> list from the <stong>instructions</strong> of the recipe,";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeOutput = document.querySelector("#recipe-output");
+  recipeOutput.classList.remove("hidden");
+  recipeOutput.innerHTML = `⏳Generating the recipe - ${userInstructionsInput.value}`;
 
   axios.get(apiURL).then(displayRecipe);
 }
