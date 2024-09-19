ce03cc2d6d9fa55950ae16dca766701e8f1bb28d
Cleo-Masson <cleonikita50@gmail.com>
2024-09-19 13:38:32 +0400

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe-output", {
-    strings: `This is your recipe`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let userInstructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "at12a73f4a384odff22ab003eefa9af8";
+  let prompt = `User instructions: Generate a quick whole foods recipe ${userInstructionsInput.value} .`;
+  let context =
+    "You are an expert in nutrition and whole food recipes AI assistant. You goal is give the user a recipe recommendation of 7 lines that is budget friendly and quick and easy. display the recipe with a title in a strong element, seperate each line with <br>. Make sure that you follow the user's instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeGeneratorElement = document.querySelector("#recipe-generator");
